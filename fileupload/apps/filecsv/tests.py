5af480f98459a67d82e99722ba34631e737b4a3d
Jose Chourio <jchouriopirela@gmail.com>
2022-06-02 10:50:27 -0400

    Added test for wrong csv file. Added validation if file is wrong

--- fileupload/apps/filecsv/tests.py
+++ fileupload/apps/filecsv/tests.py
@@ -20,3 +20,11 @@ class FileCSVTest(TestCase):
             files=files)
 
         assert response.status_code == status.HTTP_204_NO_CONTENT
+
+    def test_upload_file_wrong_format(self):
+        files = {'csvfile': open('postcodesgeo_wrong.csv', 'rb')}
+        response = requests.post(
+            'http://localhost:8000/api/filecsv/upload/postcodesgeo',
+            files=files)
+
+        assert response.status_code == status.HTTP_400_BAD_REQUEST
--- fileupload/apps/filecsv/views.py
+++ fileupload/apps/filecsv/views.py
@@ -1,14 +1,12 @@
 import time
 import os
-from pathlib import Path
 
-from django.shortcuts import render
 from django.conf import settings
 from rest_framework import views
 from rest_framework.parsers import FileUploadParser
 from rest_framework.response import Response
+from rest_framework import status
 from django.core.files.storage import FileSystemStorage
-import pandas as pd
 from django.core.files.storage import default_storage
 
 from .producer import publish
@@ -23,11 +21,20 @@ class FileUploadView(views.APIView):
         filename = fs.save(csvfile.name + f"_{time.time()}.csv", csvfile)
         uploaded_file_url = fs.url(filename)
 
-        publish("file_uploaded", {
-            'filename': filename,
-            'file_url': uploaded_file_url
-        })
+        fcsv = open(os.path.join(settings.MEDIA_ROOT, filename))
+        content = fcsv.read()
 
-        return Response(status=204, data={
-            'message': "The file has been uploaded correctly"
-        })
+        if 'lat,lon\n' in content:
+            publish("file_uploaded", {
+                'filename': filename,
+                'file_url': uploaded_file_url
+            })
+
+            return Response(status=status.HTTP_204_NO_CONTENT, data={
+                'message': "The file has been uploaded correctly"
+            })
+        else:
+            os.remove(os.path.join(settings.MEDIA_ROOT, filename))
+            return Response(status=status.HTTP_400_BAD_REQUEST, data={
+                'message': "The File has errors"
+            })
